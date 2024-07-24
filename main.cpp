2c707100ecf0f82b3d5f7c72d0fff480c1bc54ee
jk-aero <92267875+jk-aero@users.noreply.github.com>
2024-07-24 19:57:51 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -22,3 +22,42 @@ Grid(int x, int y) :posX(x), posY(y) {}
 //type 2 means its start
 //type 3 means its end
 };
+int main(){
+
+// Initialization
+//--------------------------------------------------------------------------------------
+
+InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+//--------------------------------------------------------------------------------------
+
+for (int i = 0; i < numOfCellX; i ++) {
+for (int j = 0; j < numOfCellY; j ++)
+{
+grids[i][j] = Grid{ i * cellSize,j * cellSize };
+//std::cout << grids[i][j].posX << " :x "<<grids[i][j].posY<<" :y \n";
+
+}
+};
+
+bool setStart = 0;
+bool setEnd = 0;
+float startX = 0; float startY = 0;
+float endX = 0; float endY = 0;
+
+
+
+
+// Main game loop
+while (!WindowShouldClose()) // Detect window close button or ESC key
+{
+
+
+
+// Draw
+//----------------------------------------------------------------------------------
+BeginDrawing();
+
+ClearBackground(BLACK);
+
